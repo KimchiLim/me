e14340b55070772341605fec3a62bf6773ab5452
Kevin Lim <kevin.lim6459@gmail.com>
2024-08-26 18:37:36 -0700

    moreeee changes

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -20,12 +20,12 @@ function Projects() {
 
       <p className='App-text'>
         This is my attempt at implementing the <a rel='noreferrer' target='_blank' href='https://en.wikipedia.org/wiki/Bayesian_optimization' className='App-link'>Bayesian optimization</a> algorithm for the hyperparameter tuning of a Convolutional 
-        Neural Network (CNN). Bayesian optimization is an optimization technique suited for expensive-to-evaluate functions 
-        which operates by creating a probabilistic surrogate function based on Gaussian process regression. Essentially, we 
-        probe some black box function at a few key points, which allows us to construct an estimation with confidence bounds around where we believe 
+        Neural Network (CNN). Bayesian optimization is a technique suited for optimizing expensive-to-evaluate functions 
+        which works by maximizing a probabilistic surrogate function based on Gaussian process regression. Essentially, we 
+        query some black box function at a few key points, which allows us to construct a surrogate function with confidence bounds around where we believe 
         the true function lies. We can then employ an optimization heuristic (expected improvement, upper confidence bound, etc.) on 
-        the surrogate function and probe some new point which is likely to provide beneficial information. Bayesian optimization is well-suited 
-        for optimizing hyperparameters, since model training is a computationally costly task.
+        the surrogate function and query some new point which is likely to provide beneficial information. Bayesian optimization is well-suited 
+        for optimizing neural network hyperparameters, since model training is a computationally costly task.
       </p>
 
       <img src={Picture} alt='Bayesian optimization in action' className='App-img'></img>
